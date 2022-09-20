374a2f2d8eabc045e04fe7e01da6a5cd0f097f12
icestormikk <jigalow.pavel2015@yandex.ru>
2022-09-20 16:13:08 +0300

    Add format strings to processQueries functions

--- src/main/kotlin/ru/ac/uniyar/databasescourse/DatabaseExample.kt
+++ src/main/kotlin/ru/ac/uniyar/databasescourse/DatabaseExample.kt
@@ -2,13 +2,13 @@ package ru.ac.uniyar.databasescourse
 
 import ru.ac.uniyar.databasescourse.config.DATABASE_NAME
 import ru.ac.uniyar.databasescourse.functions.FilesOperations
-import ru.ac.uniyar.databasescourse.functions.sendQueries
+import ru.ac.uniyar.databasescourse.functions.processQueries
 
 fun main(args: Array<String>) {
     println("The work has started")
-    FilesOperations.csvLinesToSqlValues("solutions.csv")
+    val values = FilesOperations.csvLinesToSqlValues("solutions.csv")
 
-    /*sendQueries(listOf(
+    processQueries(listOf(
         "USE $DATABASE_NAME",
         "SET SQL_SAFE_UPDATES = 0",
         // 1
@@ -26,21 +26,21 @@ fun main(args: Array<String>) {
     ))
 
     println("Вывод для пункта 3:")
-    sendQueries(listOf(
+    processQueries(listOf(
         "USE $DATABASE_NAME",
         // 3
         "SELECT solutions.surname AS \"Student's surname\", solutions.answer " +
-                "AS \"Student's review\" FROM solutions WHERE has_score = 0;",
+                "AS \"Student's review\" FROM solutions WHERE has_pass = 0;",
         // 4
         "INSERT INTO solutions (name,surname,card,answer) VALUES ('Алексей', 'Ефимов', '761805'," +
                 "'Пушкин, конечно, герой, но зачем стулья ломать?')",
         // 5
         "UPDATE solutions SET score = 4.1, review = 'Стулья ломать, действительно, незачем', has_pass = 1 " +
                 "WHERE card = 761805",
-    ))
+    ), "\t%s ")
 
     println("Вывод для пункта 6:")
-    sendQueries(listOf(
+    processQueries(listOf(
         "USE $DATABASE_NAME",
         // 6
         "SELECT solutions.surname AS \"Student's surname\", solutions.card AS \"Card Id\" FROM solutions " +
@@ -49,5 +49,5 @@ fun main(args: Array<String>) {
         // 7
         "DELETE FROM solutions WHERE has_pass = 0",
         "DROP TABLE solutions"
-    ))*/
+    ), "\t%s ")
 }
