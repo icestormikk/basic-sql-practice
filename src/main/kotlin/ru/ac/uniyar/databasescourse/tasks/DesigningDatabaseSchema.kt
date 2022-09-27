c7fe5d7454a3fa22e0aadf5c9ab6d30f3ea7e215
icestormikk <jigalow.pavel2015@yandex.ru>
2022-09-27 22:23:07 +0300

    Code for 2nd task

---
+++ src/main/kotlin/ru/ac/uniyar/databasescourse/tasks/DesigningDatabaseSchema.kt
@@ -0,0 +1,106 @@
+package ru.ac.uniyar.databasescourse.tasks
+
+import de.siegmar.fastcsv.reader.NamedCsvRow
+import ru.ac.uniyar.databasescourse.functions.FilesOperations.convertCSVLinesToMySQLValues
+import ru.ac.uniyar.databasescourse.functions.QueriesOperations.processQueries
+import java.util.Locale
+
+private const val STUDENTS_TABLE_NAME = "students"
+private const val REVIEWERS_TABLE_NAME = "reviewers"
+private const val SOLUTIONS_TABLE_NAME = "solutions"
+
+@SuppressWarnings("LongMethod")
+fun databaseSchemaDesign() {
+    val resultMap = mapOf(
+        STUDENTS_TABLE_NAME to mutableSetOf<String>(),
+        REVIEWERS_TABLE_NAME to mutableSetOf(),
+        SOLUTIONS_TABLE_NAME to mutableSetOf()
+    )
+
+    processQueries(
+        listOf(
+            // 1
+            "CREATE TABLE IF NOT EXISTS $STUDENTS_TABLE_NAME (\n" +
+                    "\tstudentID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
+                    "    studentName VARCHAR(255) NOT NULL,\n" +
+                    "    studentSurname VARCHAR(255) NOT NULL\n" +
+                    ");\n",
+            "CREATE TABLE IF NOT EXISTS $REVIEWERS_TABLE_NAME (\n" +
+                    "\treviewerID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
+                    "    reviewerSurname VARCHAR(255) NOT NULL,\n" +
+                    "    reviewerDepartment VARCHAR(255) NOT NULL\n" +
+                    ");",
+            "CREATE TABLE IF NOT EXISTS $SOLUTIONS_TABLE_NAME (\n" +
+                    "\tsolutionID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
+                    "    studentID INT NOT NULL,\n" +
+                    "    reviewerID INT NOT NULL,\n" +
+                    "    score float NOT NULL DEFAULT 0.0,\n" +
+                    "    hasPassed BOOLEAN,\n" +
+                    "    FOREIGN KEY (studentID) REFERENCES students (studentID) ON DELETE CASCADE,\n" +
+                    "    FOREIGN KEY (reviewerID) REFERENCES reviewers (reviewerID) ON DELETE CASCADE\n" +
+                    ")"
+        )
+    )
+
+    convertCSVLinesToMySQLValues("data.csv") {
+        // 2
+        with(resultMap) {
+            get(STUDENTS_TABLE_NAME)!!.add(
+                it.convertToSQLValueByFields(
+                    setOf("studentID", "studentName", "studentSurname")
+                )
+            )
+            get(REVIEWERS_TABLE_NAME)!!.add(
+                it.convertToSQLValueByFields(
+                    setOf("reviewerID", "reviewerSurname", "reviewerDepartment")
+                )
+            )
+            get(SOLUTIONS_TABLE_NAME)!!.add(
+                it.convertToSQLValueByFields(
+                    setOf("solutionID", "studentID", "reviewerID", "score", "hasPassed")
+                ) { title ->
+                    if (title == "hasPassed")
+                        if (it.getField(title) == "Yes") "1" else "0"
+                    else it.getField(title)
+                }
+            )
+        }
+    }
+
+    resultMap.forEach {
+        processQueries(
+            listOf("INSERT INTO ${it.key} VALUES ${it.value.joinToString(separator = ",")}")
+        )
+    }
+
+    report()
+
+    resultMap.keys.reversed().forEach {
+        processQueries(listOf("DROP TABLE $it;"))
+    }
+}
+
+private fun report() {
+    println("Work completed: added ")
+    processQueries(
+        listOf(
+            "SELECT COUNT(*) as 'students' FROM $STUDENTS_TABLE_NAME",
+            "SELECT COUNT(*) as 'reviewers' FROM $REVIEWERS_TABLE_NAME",
+            "SELECT COUNT(*) as 'solutions' FROM $SOLUTIONS_TABLE_NAME"
+        )
+    ) {
+        println("\t${it.getString(1)} ${it.metaData.getColumnLabel(1)}")
+    }
+}
+
+private fun NamedCsvRow.convertToSQLValueByFields(
+    fieldNamesList: Set<String> = this.fields.keys,
+    mapFunction: (String) -> String = { this.getField(it) },
+) = fieldNamesList.map { fieldName ->
+        mapFunction(fieldName)
+    }.joinToString(
+        prefix = "(",
+        postfix = ")",
+        separator = ", ",
+        transform = { String.format(Locale.ENGLISH, "'$it'") }
+    )
