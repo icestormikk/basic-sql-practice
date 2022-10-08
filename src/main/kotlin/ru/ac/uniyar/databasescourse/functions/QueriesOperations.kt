fbf11dbbdc8a2606614ef7404c7e9adc1b237374
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-08 21:34:57 +0300

    Changes to the old documentation and adding it for new functions

--- src/main/kotlin/ru/ac/uniyar/databasescourse/functions/QueriesOperations.kt
+++ src/main/kotlin/ru/ac/uniyar/databasescourse/functions/QueriesOperations.kt
@@ -28,8 +28,8 @@ object QueriesOperations {
      * Creates a connection to the database and sends the queries specified in the [queries] parameter to it.
      * The [callback] parameter allows you to use the result of executing the request for your own purposes
      * (by default, the function displays the result on the screen).
-     * @param queries List<String>
-     * @param callback Function2<Statement, ResultSet, Unit>
+     * @param queries list of queries in SQL format
+     * @param callback function processing the response received from the database
      */
     @SuppressWarnings("NestedBlockDepth")
     fun processQueries(
@@ -60,15 +60,25 @@ object QueriesOperations {
         }
     }
 
+    /**
+     * Generates an object of type PreparedStatement with the INSERT command and the specified parameters.
+     * @param tableTitle the name of the table in which the change will be made
+     * @param headers headers for which stubs will be created
+     * @return PreparedStatement
+     */
     fun configurePreparedStatement(
         tableTitle: String,
-        values: Collection<String>,
+        headers: Collection<String>,
     ): PreparedStatement =
         databaseConnection.prepareStatement(
-            "INSERT INTO $tableTitle (${ values.toSet().joinToString(separator = ",") }) VALUES " +
-                    "(${ CharArray(values.size) {'?'}.joinToString(separator = ",") });"
+            "INSERT INTO $tableTitle (${ headers.toSet().joinToString(separator = ",") }) VALUES " +
+                    "(${ CharArray(headers.size) {'?'}.joinToString(separator = ",") });"
         )
 
+    /**
+     * Sends a request with the INSERT command to the database.
+     * @receiver PreparedStatement
+     */
     @SuppressWarnings("SwallowedException")
     fun PreparedStatement.insertToDatabase() {
         try {
@@ -79,7 +89,7 @@ object QueriesOperations {
     }
 
     /**
-     * Default callback for the processQueries function
+     * Default callback for the processQueries function.
      * @return (Statement, ResultSet) -> Unit
      */
     private fun defaultCallback(): (Statement, ResultSet) -> Unit =
