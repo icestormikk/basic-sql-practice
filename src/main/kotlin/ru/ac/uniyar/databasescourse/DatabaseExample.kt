1e6d673d68d2a94d0e1a203faba433a65aaeaf1a
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-08 18:44:04 +0300

    Adding the ability to measure the execution time of a function

--- src/main/kotlin/ru/ac/uniyar/databasescourse/DatabaseExample.kt
+++ src/main/kotlin/ru/ac/uniyar/databasescourse/DatabaseExample.kt
@@ -1,8 +1,14 @@
 package ru.ac.uniyar.databasescourse
 
 import ru.ac.uniyar.databasescourse.tasks.databaseSchemaDesign
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
+@OptIn(ExperimentalTime::class)
 fun main() {
     println("The work has started")
-    databaseSchemaDesign()
+    val duration = measureTimedValue {
+        databaseSchemaDesign()
+    }
+    println(duration.duration)
 }
