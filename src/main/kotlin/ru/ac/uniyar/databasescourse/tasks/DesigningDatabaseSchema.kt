96e41b42890adac4c14b5c375d1f57521272d428
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-08 18:37:06 +0300

    Adding code for operating a new entity "Department"

--- src/main/kotlin/ru/ac/uniyar/databasescourse/tasks/DesigningDatabaseSchema.kt
+++ src/main/kotlin/ru/ac/uniyar/databasescourse/tasks/DesigningDatabaseSchema.kt
@@ -13,39 +13,53 @@ import java.sql.Statement
 private const val STUDENTS_TABLE_NAME = "students"
 private const val REVIEWERS_TABLE_NAME = "reviewers"
 private const val SOLUTIONS_TABLE_NAME = "solutions"
+private const val DEPARTMENT_TABLE_NAME = "departments"
 
 @SuppressWarnings("LongMethod")
 fun databaseSchemaDesign() {
-    val resultMap = mapOf(
+    val tableNameAndValues = mapOf(
         STUDENTS_TABLE_NAME to mutableSetOf<String>(),
+        DEPARTMENT_TABLE_NAME to mutableSetOf(),
         REVIEWERS_TABLE_NAME to mutableSetOf(),
         SOLUTIONS_TABLE_NAME to mutableSetOf()
     )
 
     processQueries(
-        listOf(
-            // 1
-            "CREATE TABLE IF NOT EXISTS $STUDENTS_TABLE_NAME (\n" +
-                    "\tstudentID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
-                    "    studentName VARCHAR(255) NOT NULL,\n" +
-                    "    studentSurname VARCHAR(255) NOT NULL\n" +
-                    ");\n",
-            "CREATE TABLE IF NOT EXISTS $REVIEWERS_TABLE_NAME (\n" +
-                    "\treviewerID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
-                    "    reviewerSurname VARCHAR(255) NOT NULL,\n" +
-                    "    reviewerDepartment VARCHAR(255) NOT NULL\n" +
-                    ");",
-            "CREATE TABLE IF NOT EXISTS $SOLUTIONS_TABLE_NAME (\n" +
-                    "\tsolutionID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
-                    "    studentID INT NOT NULL,\n" +
-                    "    reviewerID INT NOT NULL,\n" +
-                    "    score FLOAT NOT NULL DEFAULT 0.0,\n" +
-                    "    hasPassed BOOLEAN,\n" +
-                    "    FOREIGN KEY (studentID) REFERENCES students (studentID) ON DELETE CASCADE" +
-                    " ON UPDATE CASCADE,\n" +
-                    "    FOREIGN KEY (reviewerID) REFERENCES reviewers (reviewerID) ON DELETE CASCADE" +
-                    " ON UPDATE CASCADE\n" +
-                    ")"
+        "CREATE TABLE IF NOT EXISTS $STUDENTS_TABLE_NAME (\n" +
+                "\tstudentID INT PRIMARY KEY NOT NULL,\n" +
+                "    studentName VARCHAR(255) NOT NULL,\n" +
+                "    studentSurname VARCHAR(255) NOT NULL\n" +
+                ");\n",
+        "CREATE TABLE IF NOT EXISTS $DEPARTMENT_TABLE_NAME (\n" +
+                "    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT," +
+                "    reviewerDepartment VARCHAR(255) NOT NULL UNIQUE" +
+                ");",
+        "CREATE TABLE IF NOT EXISTS $REVIEWERS_TABLE_NAME (\n" +
+                "\treviewerID INT PRIMARY KEY NOT NULL,\n" +
+                "    reviewerSurname VARCHAR(255) NOT NULL,\n" +
+                "    departmentID INT NOT NULL,\n" +
+                "    FOREIGN KEY (departmentID) REFERENCES $DEPARTMENT_TABLE_NAME (id) " +
+                "ON DELETE RESTRICT ON UPDATE CASCADE\n" +
+                ");",
+        "CREATE TABLE IF NOT EXISTS $SOLUTIONS_TABLE_NAME (\n" +
+                "\tsolutionID INT PRIMARY KEY NOT NULL,\n" +
+                "    studentID INT NOT NULL,\n" +
+                "    reviewerID INT NOT NULL,\n" +
+                "    score FLOAT NOT NULL DEFAULT 0.0,\n" +
+                "    hasPassed BOOLEAN,\n" +
+                "    FOREIGN KEY (studentID) REFERENCES $STUDENTS_TABLE_NAME (studentID) ON DELETE RESTRICT" +
+                " ON UPDATE CASCADE,\n" +
+                "    FOREIGN KEY (reviewerID) REFERENCES $REVIEWERS_TABLE_NAME (reviewerID) ON DELETE RESTRICT" +
+                " ON UPDATE CASCADE\n" +
+                ")"
+    )
+
+    readCSVFilePerLine("data.csv") { csvRow ->
+        val tableNameToColumnNames = mapOf(
+            STUDENTS_TABLE_NAME to setOf("studentID", "studentName", "studentSurname"),
+            DEPARTMENT_TABLE_NAME to setOf("reviewerDepartment"),
+            REVIEWERS_TABLE_NAME to setOf("reviewerID", "reviewerSurname", "departmentID"),
+            SOLUTIONS_TABLE_NAME to setOf("solutionID", "studentID", "reviewerID", "score", "hasPassed")
         )
     )
 
