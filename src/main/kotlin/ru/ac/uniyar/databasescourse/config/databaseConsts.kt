c22a64737f13f3ef87e8a95e6e469311432b22fe
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-13 23:47:48 +0300

    Allocation of constants required to perform tasks in a separate file

---
+++ src/main/kotlin/ru/ac/uniyar/databasescourse/config/databaseConsts.kt
@@ -0,0 +1,6 @@
+package ru.ac.uniyar.databasescourse.config
+
+const val STUDENTS_TABLE_NAME = "students"
+const val REVIEWERS_TABLE_NAME = "reviewers"
+const val SOLUTIONS_TABLE_NAME = "solutions"
+const val DEPARTMENT_TABLE_NAME = "departments"
