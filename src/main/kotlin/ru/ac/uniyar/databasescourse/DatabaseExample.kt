610a6cd22f4848ff1d40bd9d7c2d1231d3a38329
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-15 17:23:54 +0300

    Clearing the code

--- src/main/kotlin/ru/ac/uniyar/databasescourse/DatabaseExample.kt
+++ src/main/kotlin/ru/ac/uniyar/databasescourse/DatabaseExample.kt
@@ -2,7 +2,6 @@ package ru.ac.uniyar.databasescourse
 
 import ru.ac.uniyar.databasescourse.functions.QueriesOperations.closeDatabaseConnection
 import ru.ac.uniyar.databasescourse.tasks.aggregationOperations
-import ru.ac.uniyar.databasescourse.tasks.databaseSchemaDesign
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
