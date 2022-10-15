9fda4bfe4e87ed9e7b16b3267cbc083d7f8479c9
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-15 14:55:08 +0300

    Code restructuring and assignment of tasks to separate functions

--- src/main/kotlin/ru/ac/uniyar/databasescourse/tasks/AggregationOps.kt
+++ src/main/kotlin/ru/ac/uniyar/databasescourse/tasks/AggregationOps.kt
@@ -3,62 +3,143 @@ package ru.ac.uniyar.databasescourse.tasks
 import ru.ac.uniyar.databasescourse.config.REVIEWERS_TABLE_NAME
 import ru.ac.uniyar.databasescourse.config.SOLUTIONS_TABLE_NAME
 import ru.ac.uniyar.databasescourse.config.STUDENTS_TABLE_NAME
+import ru.ac.uniyar.databasescourse.domain.task3.ReportNote
 import ru.ac.uniyar.databasescourse.functions.QueriesOperations.processQueries
 import java.sql.ResultSet
 import java.sql.Statement
 
 fun aggregationOperations() {
-    val studentsList = mutableListOf<String>()
-    val reviewersList = mutableListOf<String>()
+    val studentsList =
+        mutableListOf<Map<String, String>>()
+    val reviewersList =
+        mutableListOf<Map<String, String>>()
+    val reportList =
+        mutableListOf<Triple<String, String, Collection<Double>>>()
 
     // 1
+    taskOne(studentsList)
+    // 2
+    taskTwo(reviewersList)
+    // 3
+    taskThree(reportList)
+}
+
+private fun taskThree(reportList: MutableList<Triple<String, String, Collection<Double>>>) {
+    println("Задача 3")
     requestAndReport(
-        query = "SELECT score, $STUDENTS_TABLE_NAME.* FROM $STUDENTS_TABLE_NAME INNER JOIN $SOLUTIONS_TABLE_NAME " +
-                "ON $SOLUTIONS_TABLE_NAME.studentID = $STUDENTS_TABLE_NAME.studentID " +
-                "ORDER BY $SOLUTIONS_TABLE_NAME.score DESC;",
-        targetList = studentsList,
-        reportFunction = {
-            it.apply {
-                println(
-                    "Студент, получивший наивысшую оценку:\n\t${first()}\n" +
-                            "Студент, получивший наименьшую оценку:\n\t${last()}"
-                )
-            }
+        query = "select distinct reviewers.reviewerSurname, solutions.studentID, " +
+                "group_concat(solutions.score) as 'scores'\n" +
+                "from reviewers, solutions \n" +
+                "where reviewers.reviewerID = solutions.reviewerID\n" +
+                "group by reviewerSurname, studentID;",
+        targetList = reportList,
+        savingFunction = { _: Statement, resultSet: ResultSet ->
+            @Suppress("UNCHECKED_CAST")
+            reportList.add(
+                with(resultSet) {
+                    Triple(
+                        getString("reviewerSurname"),
+                        getString("studentID"),
+                        getString("scores").split(",") as Collection<Double>
+                    )
+                }
+            )
+        },
+        reportFunction = { list ->
+            list.groupBy { triple -> triple.first }
+                .map { entry ->
+                    ReportNote(
+                    reviewerSurname = entry.key,
+                    studentAndMarks = entry.value.map { Pair(it.second, it.third) }
+                ) }
+                .forEach { reportNote ->
+                    println("Преподаватель: ${reportNote.reviewerSurname}")
+                    reportNote.studentAndMarks.forEach {
+                        println("\tИдентификатор студента: ${it.first} <-> Оценки: ${it.second}")
+                    }
+                }
         }
     )
+}
 
-    // 2
+private fun taskTwo(reviewersList: MutableList<Map<String, String>>) {
+    println("Задание 2")
     requestAndReport(
-        query = "SELECT score, $REVIEWERS_TABLE_NAME.* FROM $SOLUTIONS_TABLE_NAME INNER JOIN $REVIEWERS_TABLE_NAME " +
-                "ON $SOLUTIONS_TABLE_NAME.reviewerID = $REVIEWERS_TABLE_NAME.reviewerID " +
-                "ORDER BY $SOLUTIONS_TABLE_NAME.score DESC;",
+        query = "SELECT $REVIEWERS_TABLE_NAME.*, AVG($SOLUTIONS_TABLE_NAME.score) AS 'average' " +
+                "FROM $SOLUTIONS_TABLE_NAME \n" +
+                "INNER JOIN $REVIEWERS_TABLE_NAME " +
+                "ON $REVIEWERS_TABLE_NAME.reviewerID = $SOLUTIONS_TABLE_NAME.reviewerID \n" +
+                "GROUP BY reviewerID \n" +
+                "ORDER BY AVG(score) DESC;",
         targetList = reviewersList,
+        savingFunction = { _: Statement, resultSet: ResultSet ->
+            reviewersList.add(
+                (1..resultSet.metaData.columnCount).associate {
+                    Pair(resultSet.metaData.getColumnLabel(it), resultSet.getString(it))
+                }
+            )
+        },
         reportFunction = {
             it.apply {
                 println(
-                    "Преподаватель, поставивший наивысшую оценку:\n\t${first()}\n" +
-                            "Преподаватель, поставивший наименьшую оценку:\n\t${last()}"
+                    with(first()) {
+                        """Преподаватель, ставивший максимальные оценки: ${this["reviewerSurname"]}
+                            |   Средняя оценка: ${this["average"]}
+                            |""".trimMargin()
+                    } +
+                    with(last()) {
+                        """Преподаватель, ставивший минимальные оценки: ${this["reviewerSurname"]}
+                    |   Средняя оценка: ${this["average"]}
+                    |""".trimMargin()
+                    }
                 )
             }
         }
     )
 }
 
-private fun requestAndReport(
-    query: String,
-    targetList: MutableCollection<String>,
-    reportFunction: (MutableCollection<String>) -> Unit
-) {
-    processQueries(query) { _: Statement, resultSet: ResultSet ->
-        targetList.add(
-            (1..resultSet.metaData.columnCount).joinToString(
-                transform = {
-                    with(resultSet.metaData.getColumnLabel(it)) {
-                        "$this - ${resultSet.getString(it)}"
-                    }
+private fun taskOne(studentsList: MutableList<Map<String, String>>) {
+    println("Задание 1")
+    requestAndReport(
+        query = "SELECT score, $STUDENTS_TABLE_NAME.* FROM $STUDENTS_TABLE_NAME " +
+                "INNER JOIN $SOLUTIONS_TABLE_NAME " +
+                "ON $SOLUTIONS_TABLE_NAME.studentID = $STUDENTS_TABLE_NAME.studentID " +
+                "ORDER BY $SOLUTIONS_TABLE_NAME.score DESC;",
+        targetList = studentsList,
+        savingFunction = { _: Statement, resultSet: ResultSet ->
+            studentsList.add(
+                (1..resultSet.metaData.columnCount).associate {
+                    Pair(resultSet.metaData.getColumnLabel(it), resultSet.getString(it))
                 }
             )
-        )
+        },
+        reportFunction = {
+            it.apply {
+                println(
+                    with(first()) {
+                        """Студент, получивший максимальную оценку: ${this["studentName"]} ${this["studentSurname"]}
+                            |   Оценка: ${this["score"]}
+                            |""".trimMargin()
+                    } +
+                    with(last()) {
+                        """Студент, получивший минимальную оценку: ${this["studentName"]} ${this["studentSurname"]}
+                    |   Оценка: ${this["score"]}
+                    |""".trimMargin()
+                    }
+                )
+            }
+        }
+    )
+}
+
+private fun <T> requestAndReport(
+    query: String,
+    targetList: T,
+    savingFunction: (Statement, ResultSet) -> Unit,
+    reportFunction: (T) -> Unit
+) {
+    processQueries(query) { statement: Statement, resultSet: ResultSet ->
+        savingFunction(statement, resultSet)
     }
     reportFunction(targetList)
 }
